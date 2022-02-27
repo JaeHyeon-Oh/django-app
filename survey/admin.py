6c235ef74dc24eeb41f531de99dd14a5a06091b0
Jaehyeon Oh <dhwogus1212@naver.com>
2022-02-27 12:19:30 +0900

    list customization add

--- survey/admin.py
+++ survey/admin.py
@@ -12,6 +12,9 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date']}),
     ]
 
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
     inlines=[ChoiceInline]
-    
+
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 admin.site.register(Question, QuestionAdmin)
