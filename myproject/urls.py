5111d383bd940393ae8bcd0f2929f1e636c43c97
Jaehyeon Oh <dhwogus1212@naver.com>
2022-02-27 16:26:16 +0900

    Bookmark function implement

--- myproject/urls.py
+++ myproject/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
-    path('survey/', include('survey.urls'))
+    path('survey/', include('survey.urls')),
+    path('bookmark/', include('bookmark.urls')),
 ]
